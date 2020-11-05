1375d7a148e4c42f1ffa8060a7405c2f12d9e039
daljeete123 <64042650+daljeete123@users.noreply.github.com>
2020-11-05 17:14:03 +0530

    Update Ide (19).cpp

--- Ide (19).cpp
+++ Ide (19).cpp
@@ -6,21 +6,21 @@ int count( int S[], int m, int n )
 { 
     int i, j, x, y; 
   
-    int table[n + 1][m]; 
+    int dp[n + 1][m]; 
       
     for (i = 0; i < m; i++) 
-        table[0][i] = 1; 
+        dp[0][i] = 1; 
   
     for (i = 1; i < n + 1; i++) 
     { 
         for (j = 0; j < m; j++) 
         {
-            x = (i-S[j] >= 0) ? table[i - S[j]][j] : 0; 
-            y = (j >= 1) ? table[i][j - 1] : 0; 
-            table[i][j] = x + y; 
+            x = (i-S[j] >= 0) ? dp[i - S[j]][j] : 0; 
+            y = (j >= 1) ? dp[i][j - 1] : 0; 
+            dp[i][j] = x + y; 
         } 
     } 
-    return table[n][m - 1]; 
+    return dp[n][m - 1]; 
 } 
  
 int main(){
